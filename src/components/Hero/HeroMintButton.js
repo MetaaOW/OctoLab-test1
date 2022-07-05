de26811cb0ca893161e42918af4fddd383bbd05e
MetaaOW <german.bayn@gmail.com>
2022-07-05 14:04:00 +0300

    22

--- src/components/Hero/HeroMintButton.js
+++ src/components/Hero/HeroMintButton.js
@@ -1,10 +1,10 @@
 export default function HeroMintButton({ mintOctoLab }) {
 	return (
 		<button
-			className="px-4 py-2 mt-2 mb-6 text-center text-4xl text-white uppercase bg-blue-500 rounded font-flower hover:bg-red-500 hover:border-blue-500"
+			className="px-4 py-2 mt-2 mb-6 text-center text-3xl text-white uppercase bg-blue-500 rounded font-flower hover:bg-red-500 hover:border-blue-500"
 			onClick={mintOctoLab}
 		>
-			{"MINT NOW!"}
+			{"0.005 PUBLIC MINT!"}
 		</button>
 	);
 }
