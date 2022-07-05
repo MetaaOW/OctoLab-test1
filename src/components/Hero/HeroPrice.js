05af46ff7cbc5962a7ab2a2fc79d8503787bb32c
MetaaOW <german.bayn@gmail.com>
2022-07-05 13:40:08 +0300

    21

--- src/components/Hero/HeroPrice.js
+++ src/components/Hero/HeroPrice.js
@@ -1,6 +1,6 @@
 export default function HeroPrice({ nftPrice }) {
 	return (
-		<h4 className="mt-2 text-2xl font-semibold text-center text-black font-flower">
+		<h4 className="mt-2 text-3xl font-semibold text-center text-black font-flower">
 			{nftPrice} ETH
 			<span className="text-2xl text-black font-flower"> + gas</span>
 		</h4>
